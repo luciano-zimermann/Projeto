5ae7b6e2ce8dc184d65ca1a76b98f6e263820c14
luciano-zimermann <lucianozn16@gmail.com>
2021-05-12 22:32:39 -0300

    Adicionado Hello World

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,9 @@ public class Program {
      */
     public static void main(String[] args) {
         
-        System.out.println("Teste");
+        System.out.println( "Teste" );
+        
+        System.out.println( "Hello World!" );
         
     }
     
